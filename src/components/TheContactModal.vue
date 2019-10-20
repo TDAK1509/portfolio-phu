29c03f5828cccb95aec28839b8037d38b5f856b8
TDAK <dbcgdhh@gmail.com>
2019-10-20 15:20:54 +0700

    Contact modal

---
+++ src/components/TheContactModal.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="modal-container" ref="modal" @click="onModalClick">
+        <div class="modal exclude">
+            <span class="text exclude">Thank you for your love letter!</span>
+            <button class="close exclude" type="button" @click="closeModal">+</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TheContactModal",
+
+    methods: {
+        closeModal() {
+            this.$emit("close");
+        },
+
+        onModalClick(event) {
+            if (!event.target.classList.contains("exclude")) {
+                this.closeModal();
+            }
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.modal-container {
+    height: 100vh;
+    width: 100%;
+    background: rgba(0, 0, 0, 0.4);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    animation: fadeIn 200ms ease-in;
+
+    .modal {
+        box-sizing: border-box;
+        background: #fff;
+        padding: 20px 40px;
+        line-height: 2rem;
+        border-radius: 4px;
+        box-shadow: 10px 10px 10px 0 #000;
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        animation: scaleIn 200ms ease-in;
+
+        .close {
+            border: none;
+            background: none;
+            font-size: 2rem !important;
+            letter-spacing: 0;
+            position: absolute;
+            top: 0px;
+            right: 7px;
+            transform: rotate(45deg);
+            cursor: pointer;
+        }
+    }
+}
+
+@media only screen and (max-width: 500px) {
+    .modal {
+        max-width: 300px;
+    }
+}
+
+@keyframes fadeIn {
+    from {
+        opacity: 0;
+    }
+
+    to {
+        opacity: 1;
+    }
+}
+
+@keyframes scaleIn {
+    from {
+        transform: scale(0);
+    }
+
+    to {
+        transform: scale(1);
+    }
+}
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -10,61 +10,221 @@
             </header>
 
             <form class="form" @submit.prevent="mail">
-                <label class="label">Name</label>
-                <input class="input form-name" type="text" />
+                <label class="label">Name*</label>
 
-                <label class="label">Email Address</label>
-                <input class="input form-email" type="email" name="email" />
+                <input
+                    class="input form-name"
+                    type="text"
+                    v-model="name"
+                    required
+                    @blur="validateName"
+                    ref="name"
+                />
 
-                <label class="label">Message</label>
-                <textarea class="input form-textarea"></textarea>
+                <div class="error">{{errorName}}</div>
+
+                <label class="label">Email Address*</label>
+
+                <input
+                    class="input form-email"
+                    type="email"
+                    name="email"
+                    v-model="email"
+                    required
+                    @blur="validateEmail"
+                    ref="email"
+                />
+
+                <div class="error">{{errorEmail}}</div>
+
+                <label class="label">Message*</label>
+
+                <textarea
+                    class="input form-textarea"
+                    v-model="message"
+                    required
+                    @blur="validateMessage"
+                    ref="message"
+                ></textarea>
+
+                <div class="error">{{errorMessage}}</div>
 
                 <button class="submit">
-                    <template v-if="!sending">
+                    <template v-if="!loading">
                         <font-awesome-icon class="icon" icon="paper-plane"></font-awesome-icon>
                         <span class="text">SEND</span>
                     </template>
 
                     <font-awesome-icon v-else class="loading" icon="spinner" pulse></font-awesome-icon>
                 </button>
+
+                <div class="error">{{formErrorMessage}}</div>
             </form>
         </main>
+
+        <TheContactModal v-if="showModal" @close="showModal = false"></TheContactModal>
     </MainContainer>
 </template>
 
 <script>
+import TheContactModal from "@/components/TheContactModal";
+
 export default {
     name: "Contact",
 
+    components: {
+        TheContactModal
+    },
+
     data() {
         return {
-            sending: false
+            loading: false,
+
+            name: "",
+            email: "",
+            message: "",
+
+            errorName: "",
+            errorEmail: "",
+            errorMessage: "",
+
+            formErrorMessage: "",
+
+            showModal: false
         };
     },
 
-    methods: {
-        mail() {
-            console.log("send");
+    computed: {
+        isFormValid() {
+            return (
+                !this.errorName &&
+                !this.errorEmail &&
+                !this.errorMessage &&
+                !!this.name &&
+                !!this.email &&
+                !!this.message
+            );
+        }
+    },
 
+    methods: {
+        isEmail(email) {
+            const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(email).toLowerCase());
+        },
+
+        validateName() {
+            const ref = this.$refs.name;
+
+            if (this.name == "") {
+                ref.classList.add("invalid");
+                this.errorName = "This field is required.";
+                this.shakeItOff(ref);
+            } else {
+                ref.classList.remove("invalid");
+                this.errorName = "";
+            }
+        },
+
+        validateMessage() {
+            const ref = this.$refs.message;
+
+            if (this.message == "") {
+                ref.classList.add("invalid");
+                this.errorMessage = "This field is required.";
+                this.shakeItOff(ref);
+            } else {
+                ref.classList.remove("invalid");
+                this.errorMessage = "";
+            }
+        },
+
+        validateEmail() {
+            const ref = this.$refs.email;
+
+            if (this.email === "") {
+                ref.classList.add("invalid");
+                this.errorEmail = "This field is required.";
+                this.shakeItOff(ref);
+            } else if (!this.isEmail(this.email)) {
+                ref.classList.add("invalid");
+                this.errorEmail = "Invalid email format.";
+                this.shakeItOff(ref);
+            } else {
+                ref.classList.remove("invalid");
+                this.errorEmail = "";
+            }
+        },
+
+        validateAll() {
+            this.validateName();
+            this.validateEmail();
+            this.validateMessage();
+        },
+
+        async mail() {
             const data = {
-                email: "dbcgdhh@gmail.com",
-                message: "test"
+                name: this.name,
+                email: this.email,
+                message: this.message
             };
 
-            fetch("https://formspree.io/xyyggwzd", {
-                method: "POST", // *GET, POST, PUT, DELETE, etc.
-                mode: "cors", // no-cors, *cors, same-origin
+            this.loading = true;
+
+            try {
+                if (!this.isFormValid) throw "1";
+
+                const response = await this.sendEmailToAPI(data);
+
+                if (!response) throw "2";
+
+                this.showThankyouModal();
+            } catch (e) {
+                if (e == "1") {
+                    this.validateAll();
+                } else {
+                    this.formErrorMessage =
+                        "Unexpected error, please try again later!";
+                    console.log("Error sending email:", e);
+                }
+            } finally {
+                this.loading = false;
+            }
+        },
+
+        async sendEmailToAPI(data) {
+            const response = await fetch("https://formspree.io/xyyggwzd", {
+                method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify(data)
             });
+
+            if (response.status !== 200) {
+                return false;
+            }
+
+            return true;
+        },
+
+        showThankyouModal() {
+            this.showModal = true;
+        },
+
+        shakeItOff(el) {
+            el.classList.add("shake");
+            setTimeout(() => {
+                el.classList.remove("shake");
+            }, 500);
         }
     }
 };
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/scss/animations.scss";
+
 .main {
     display: flex;
     justify-content: center;
@@ -106,17 +266,27 @@ export default {
         border: 1px solid #ccc;
         padding: 10px 10px;
         border-radius: 3px;
-        margin-bottom: 15px;
         font-size: 1rem;
 
         &.form-textarea {
             height: 200px;
+        }
+
+        &.invalid {
+            border-color: red;
         }
     }
 
     .label {
         font-size: 0.9rem;
         margin-bottom: 0.4rem;
+    }
+
+    .error {
+        font-size: 0.9rem;
+        color: red;
+        margin-top: 0.2rem;
+        margin-bottom: 0.8rem;
     }
 
     .submit {
@@ -165,7 +335,8 @@ export default {
         width: 300px;
         padding-top: 10px;
 
-        .label {
+        .label,
+        .error {
             font-size: 0.8rem;
         }
 
