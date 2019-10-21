c031f0f82ade95961706234e4c8b313805d123d5
TDAK <dbcgdhh@gmail.com>
2019-10-21 09:02:02 +0700

    Remove init global components

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import registerGlobalComponents from "./register-global-components";
+
 import initFontAwesome from "./init-font-awesome";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 Vue.config.productionTip = false;
-registerGlobalComponents();
+
 initFontAwesome();
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
