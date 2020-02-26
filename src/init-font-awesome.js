ccbe6bd91efe764a2537162338ba28504dfc694f
TDAK <dbcgdhh@gmail.com>
2020-02-26 19:22:47 +0700

    refactor: contact

--- src/init-font-awesome.js
+++ src/init-font-awesome.js
@@ -1,13 +1,22 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
-    faAngleRight,
-    faHandPointLeft,
-    faPaperPlane,
-    faSpinner
+  faAngleRight,
+  faHandPointLeft,
+  faPaperPlane,
+  faSpinner,
+  faEnvelope,
+  faMobileAlt
 } from "@fortawesome/free-solid-svg-icons";
 
 const initFontAwesome = function() {
-    library.add(faAngleRight, faHandPointLeft, faPaperPlane, faSpinner);
+  library.add(
+    faAngleRight,
+    faHandPointLeft,
+    faPaperPlane,
+    faSpinner,
+    faEnvelope,
+    faMobileAlt
+  );
 };
 
 export default initFontAwesome;
